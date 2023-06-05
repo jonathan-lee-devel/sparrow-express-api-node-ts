0e2cb6bdb0a9935dfab10bd7925c91a4ac706e4a
Jonathan Lee <jonathan.lee.devel@gmail.com>
2023-06-05 20:30:51 +0100

    Removed references to clipboard

--- src/main/config/logger/logger-config.ts
+++ src/main/config/logger/logger-config.ts
@@ -1,10 +1,11 @@
 import bunyan from 'bunyan';
 
-const logger: bunyan = bunyan.createLogger({name: 'clipboard-api', src: true});
+const logger: bunyan = bunyan.createLogger({name: 'sparrow-express-api', src: true});
 logger.info('Initializing application');
 
 /**
  * Bunyan logger configuration.
+ * @return {bunyan} logger
  */
 export const loggerConfig = () => {
   return logger;
